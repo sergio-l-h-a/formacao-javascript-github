26742ae74a2b42a30eb92683199d1f3c9510e884
sergio-l-h-a <lanhousearte@outlook.com>
2025-09-09 17:56:53 +0000

    Atualizando arquivos main.js e poke-api.js

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,13 +1,14 @@
 function converterPokemonToLi(pokemon) {
+    console.log(pokemon)
     const types = pokemon.types.map(type => `<li class="type">${type.type.name}</li>`).join('')
 
     return `
     <li class="pokemon">
         <span class="number">#${pokemon.id.toString().padStart(3, '0')}</span>
-        <span class="name">${pokemon.name}}</span>
+        <span class="name">${pokemon.name}</span>
         <div class="detail">
-            <ol clas="types">${types}</ol>
-            <img src="${pokemon.sorites.other.dream_world.front_default}" alt="${pokemon.name}" />
+            <ol class="types">${types}</ol>
+            <img src="${pokemon.sprites.other.dream_world.front_default}" alt="Pokemon ${pokemon.name} stands in a neutral pose. Types: ${pokemon.types.map(type => type.type.name).join(', ')}. Number ${pokemon.id.toString().padStart(3, '0')} and name ${pokemon.name} are displayed. The environment is a digital card layout with a calm tone." />
         </div>
     </li>
     `
--- pokedex/assets/js/poke-api.js
+++ pokedex/assets/js/poke-api.js
@@ -1,12 +1,23 @@
-const pokeApi = {}
+ const pokeApi = {}
+
+// pokeApi.getPokemons = (offset = 0, limit = 10) => {
+//        const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+//        return fetch(url)
+//                 .then((response) => response.json())
+//                 .then((jsonBody) => jsonBody.results)
+//                 .then((pokemons) => pokemons.map(pokemon => fetch(pokemon.url)
+//                 .then((res => res.json()))
+//                 .catch((error) => console.error('Erro ao buscar pokemon: ', error))))
+// }
 
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
-       const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-       return fetch(url)
-                .then((response) => response.json())
-                .then((jsonBody) => jsonBody.results)
-                .then((pokemons) => pokemons.map(pokemon => fetch(pokemon.url)
-                .then((res => res.json()))
-                .catch((error) => console.error('Erro ao buscar pokemon: ', error))))
+  return fetch(url)
+    .then(response => response.json())
+    .then(jsonBody => jsonBody.results)
+    .then(pokemonList => Promise.all(
+      pokemonList.map(pokemon => fetch(pokemon.url).then(res => res.json()))
+    ))
 }
