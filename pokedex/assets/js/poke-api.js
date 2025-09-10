2be5dc72be298df25b3a4513252a8ec0ddcb08ff
sergio-l-h-a <lanhousearte@outlook.com>
2025-09-10 17:08:38 +0000

    Alterando projeto

--- pokedex/assets/js/poke-api.js
+++ pokedex/assets/js/poke-api.js
@@ -10,3 +10,21 @@ pokeApi.getPokemons = (offset = 0, limit = 10) => {
       pokemonList.map(pokemon => fetch(pokemon.url).then(res => res.json()))
     ))
 }
+
+
+/*
+ const pokeApi = {}
+
+  pokeApi.getPokemons = async (offset = 0, limit = 10) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+    const response = await fetch(url)
+    const jsonBody = await response.json()
+    const pokemonList = jsonBody.results
+    return await Promise.all(
+      pokemonList.map(pokemon => fetch(pokemon.url).then(res => res.json()))
+    )
+}
+
+
+*/
